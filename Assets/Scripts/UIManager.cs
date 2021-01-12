eaaa9a22b331abf779ea4ea706fb946c4e22ab38
Tony Zito <tonyzito@gmail.com>
2021-01-12 10:34:32 -0800

    added some bits to make images zoomable, still not working right

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class UIManager : Singleton<UIManager>
+{
+
+    [SerializeField]
+    private Canvas mainCanvas;  
+
+    [SerializeField]
+    private GameObject actionsPanel;
+
+    [SerializeField]
+    private GameObject endTurnButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetActionsPanelVisibility(bool visible)
+    {
+        actionsPanel.SetActive(visible);
+        endTurnButton.SetActive(!visible);
+    }
+
+
+}
---
+++ Assets/Scripts/Zoomable.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Zoomable : MonoBehaviour, IPointerClickHandler
+{
+
+    bool mainCamActive = true;
+    public GameObject zoomCam;
+    Camera mainCam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        mainCam = Camera.main;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!mainCamActive) 
+        {
+            if (Input.GetMouseButton(0) || Input.GetKeyDown("escape"))
+            {
+                SwapCameras();
+            }
+        }
+        
+    }
+
+    public void OnPointerClick(PointerEventData e)
+    {
+        SwapCameras();
+    }
+
+    void SwapCameras()
+    {
+        mainCamActive = !mainCamActive;
+        mainCam.enabled = mainCamActive;
+        zoomCam.SetActive(!mainCamActive);
+        Debug.Log("mousedown on image");
+    }
+}
